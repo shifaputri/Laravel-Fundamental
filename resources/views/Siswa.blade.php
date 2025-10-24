11574bd6e06a7f36304561d742d6d3f4582b6927
shifasaputri <putrishifaa31@gmail.com>
2025-10-24 13:35:30 +0700

    migration add table

---
+++ resources/views/Siswa.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>siswa laravel</title>
+</head>
+<body>
+    <h1>Halo, ini halaman siswa!</h1>
+</body>
+</html>
